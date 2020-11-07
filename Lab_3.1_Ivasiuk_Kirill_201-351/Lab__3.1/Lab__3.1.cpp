c547e5a94f9054e65beac0bd84dd5ac80ae07c97
Ивасюк Кирилл <72147433+IvasiukKirill@users.noreply.github.com>
2020-11-07 09:57:05 +0300

    a

--- Lab_3.1_Ivasiuk_Kirill_201-351/Lab__3.1/Lab__3.1.cpp
+++ Lab_3.1_Ivasiuk_Kirill_201-351/Lab__3.1/Lab__3.1.cpp
@@ -1,12 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
-const size_t len = 1000;
+const size_t length = 1000; 
+/* объявление глобальной переменной,
+Тип size_t применяется для счетчиков циклов, индексации массивов,
+хранения размеров, адресной арифметики
+*/
 
-void BoubleSort(int* arr, const size_t len) {
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+void BoubleSort(int *arr, const size_t length) 
+{
+    for (int i = 0; i < length; i++) 
+    {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
                 int buff = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = buff;
@@ -15,88 +23,110 @@ void BoubleSort(int* arr, const size_t len) {
     }
 }
 
-template < typename T >
-void PrintArray(T* arr, const size_t n) {
-    for (int i = len - n; i < len; i++) {
+template < typename T > 
+/* шаблон типа
+Ключевое слово typename говорит о том, 
+что в шаблоне будет использоваться встроенный
+тип данных, такой как: int, double, float, char и т. д.
+*/
+
+void PrintArray(T *arr, const size_t n)
+{
+    for (int i = length - n; i < length; i++) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-void CountSort(char* arr, const size_t len) {
+void CountSort(char* arr, const size_t length)
+{
     const int k = 26;
     int counter[k] = { 0 };
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < length; i++)
+    {
         counter[int(arr[i] - 'a')]++;
     }
     int pos = 0;
     int j = 0;
     while (j <= k) {
-        if (counter[j] > 0) {
+        if (counter[j] > 0)
+        {
             arr[pos] = char(int('a') + j);
             pos++;
             counter[j]--;
         }
-        else {
+        else 
+        {
             j++;
         }
     }
 }
 
-void Merge(int arr[], const int left, const int mid, const int right) {
-    const int size1 = mid - left + 1;
-    const int size2 = right - mid;
-    int up[len / 2], down[len - len / 2];
+void Merge(int *arr,  int left,  int mid,  int right)
+{
+    int size1 = mid - left + 1;
+    int size2 = right - mid;
+    int up[length / 2], down[length - length / 2];
 
-    for (int i = 0; i < size1; i++) {
+    for (int i = 0; i < size1; i++) 
+    {
         up[i] = arr[left + i];
     }
-    for (int i = 0; i < size2; i++) {
+    for (int i = 0; i < size2; i++) 
+    {
         down[i] = arr[mid + i + 1];
     }
 
-    int i = 0;
-    int j = 0;
-    int k = left;
+    int i = 0,j = 0,k = left;
 
-    while (i < size1 && j < size2) {
-        if (up[i] <= down[j]) {
+    while (i < size1 && j < size2) 
+    {
+        if (up[i] <= down[j]) 
+        {
             arr[k] = up[i];
             i++;
         }
-        else {
+        else 
+        {
             arr[k] = down[j];
             j++;
         }
         k++;
     }
 
-    while (i < size1) {
+    while (i < size1)
+    {
         arr[k] = up[i];
         i++;
         k++;
     }
 
-    while (j < size2) {
+    while (j < size2)
+    {
         arr[k] = down[j];
         j++;
         k++;
     }
 }
 
-void MergeSort(int arr[], const int left, const int right) {
-    if (left < right) {
+void MergeSort(int *arr, const int left, const int right)
+{
+    if (left < right)
+    {
         int mid = (left + right) / 2;
-        MergeSort(arr, left, mid);
-        MergeSort(arr, mid + 1, right);
+        MergeSort(arr, left, mid); // cортировка первой части
+        MergeSort(arr, mid + 1, right); //второй
 
-        Merge(arr, left, mid, right);
+        Merge(arr, left, mid, right);// слияние частей
     }
 }
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "Russian");
-    while (true) {
+    while (true) 
+    {
         int number;
         cout << "Выберите команду:\n"
             << "1.Сортировка пузырьком\n"
@@ -106,17 +136,19 @@ int main() {
         cin >> number;
 
 
-        if (number == 1) {
+        if (number == 1)
+        {
             int n = 0;
             cout << "Размер массива: ";
             cin >> n;
 
-            int arr[len] = { 0 };
-            for (int i = 0; i < n; i++) {
+            int arr[length] = { 0 };
+            for (int i = 0; i < n; i++)
+            {
                 cin >> arr[i];
             }
 
-            BoubleSort(arr, len);
+            BoubleSort(arr, length);
             PrintArray(arr, n);
         }
 
@@ -125,30 +157,34 @@ int main() {
             cout << "Размер массива: ";
             cin >> n;
 
-            char arr[len] = { ' ' };
-            for (int i = 0; i < n; i++) {
+            char arr[length] = { ' ' };
+            for (int i = 0; i < n; i++)
+            {
                 cin >> arr[i];
             }
 
-            CountSort(arr, len);
+            CountSort(arr, length);
             PrintArray(arr, n);
         }
 
-        if (number == 3) {
+        if (number == 3)
+        {
             int n = 0;
             cout << "Размер массива: ";
             cin >> n;
 
-            int arr[len] = { 0 };
-            for (int i = 0; i < n; i++) {
+            int arr[length] = { 0 };
+            for (int i = 0; i < n; i++)
+            {
                 cin >> arr[i];
             }
 
-            MergeSort(arr, 0, len - 1);
+            MergeSort(arr, 0, length - 1);
             PrintArray(arr, n);
         }
 
-        if (number == 4) {
+        if (number == 4)
+        {
             return 0;
         }
     }
