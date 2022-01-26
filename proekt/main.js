7216cf219d4740712951db759b87001990f4e58a
Ивасюк Кирилл <72147433+IvasiukKirill@users.noreply.github.com>
2022-01-26 23:12:00 +0300

    фильтрация
    Работающая фильтрация ресторанов

--- proekt/main.js
+++ proekt/main.js
@@ -23,13 +23,83 @@ function createRestaurantTableItem(record) {
 }
 
 function renderRecords(records) {
-    let restaurantTable = document.querySelector("tbody");
-    for (let i = 0; i < records.length; i++) {
-        restaurantTable.append(createRestaurantTableItem(records[i]));
-    }
+  let restaurantTable = document.querySelector("tbody");
+  for (let i = 0; i < records.length; i++) {
+    restaurantTable.append(createRestaurantTableItem(records[i]));
+  }
+  getFilter();
 }
 
-// window.onload = function () {
-//     getRestaurants().then(renderRecords);
-   
-// }
+
+window.onload = function () {
+  getRestaurants().then(renderRecords);
+  document.querySelector("#find").onclick = filterRecords;
+};
+
+
+
+function filterRecords() {
+  let selectedAdm = document.getElementById("area").value;
+  let selectedDistrict = document.getElementById("district").value;
+  let selectedType = document.getElementById("type").value;
+  let selectedDiscount = document.getElementById("discount").value;
+
+  let restaurantTable = document.querySelector("tbody");
+  while (restaurantTable.children.length > 1) {
+    restaurantTable.removeChild(restaurantTable.lastChild);
+  }
+    
+  for (let i = 0; i < restaurantsJson.length; i++) {
+    if (
+        
+      (selectedAdm == "Not selected" ||
+        selectedAdm == restaurantsJson[i].admArea) &&
+      (selectedDistrict == "Not selected" ||
+        selectedDistrict == restaurantsJson[i].district) &&
+      (selectedDiscount == "Not selected" ||
+        selectedDiscount == restaurantsJson[i].socialPrivileges) &&
+      (selectedType == "Not selected" ||
+        selectedType == restaurantsJson[i].typeObject)
+    ) {
+      restaurantTable.append(createRestaurantTableItem(restaurantsJson[i]));
+    }
+  }
+}
+
+function getFilter() {
+  var arrType = [];
+  for (let i = 0; i < restaurantsJson.length; i++) {
+    arrType.push(restaurantsJson[i].typeObject);
+  }
+  var unarr = new Set(arrType);
+
+  for (let value of unarr) {
+    let qwe = document.createElement("option");
+    qwe.innerHTML = value;
+    document.querySelector("#type").appendChild(qwe);
+  }
+
+  var arrAdm = [];
+  for (let i = 0; i < restaurantsJson.length; i++) {
+    arrAdm.push(restaurantsJson[i].admArea);
+  }
+  var unarr = new Set(arrAdm);
+
+  for (let value of unarr) {
+    let qwe = document.createElement("option");
+    qwe.innerHTML = value;
+    document.querySelector("#area").appendChild(qwe);
+  }
+
+  var arrDistrict = [];
+  for (let i = 0; i < restaurantsJson.length; i++) {
+    arrDistrict.push(restaurantsJson[i].district);
+  }
+  var unarr = new Set(arrDistrict);
+
+  for (let value of unarr) {
+    let qwe = document.createElement("option");
+    qwe.innerHTML = value;
+    document.querySelector("#district").appendChild(qwe);
+  }
+}
